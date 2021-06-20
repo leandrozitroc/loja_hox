d185eb1ba46664ee62ee69e931e6e8e3b36d3a90
leandro.cortiz <leandro.cortiz@fcamara.com.br>
2021-06-20 15:32:45 -0300

    access

--- index.ts
+++ index.ts
@@ -2,43 +2,45 @@ import * as bodyParser from "body-parser";
 import * as express from "express";
 
 import { dbConfig } from "./Database/database";
-import CreateAcc1 from './routes/createAcoount'
-import Login from './routes/login'
-import AuthJwt from './middleware/Authmid' //middleware para jwt token(nao utilizado para testes)
-import Category from "./routes/categories"
-import Products from "./routes/produts"
+import CreateAcc1 from "./routes/createAcoount";
+import Login from "./routes/login";
+
+import Category from "./routes/categories";
+import Products from "./routes/produts";
+
+import AdminLogin from "./routes/Admin";
 const app = express();
 app.use(require("cors")());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-/*dbConfig.sync({force:true}).then(()=>{
+/*dbConfig.sync({force:false}).then(()=>{
     console.log('Everything ok')//(USADO PARA CRIAR OS DBs)
 })*/
-app.get('/', (req,res)=>{
-    res.send("hello Loja Hox")
-})
+app.get("/", (req, res) => {
+  res.send("hello Loja Hox");
+});
 //login/account
-app.post('/createAccount', CreateAcc1.createacc)
-app.post('/login', Login.trueLogin)
+app.post("/createAccount", CreateAcc1.createacc);
+app.post("/login", Login.trueLogin);
 //categories
-app.post('/addCategory', Category.addCategory)
-app.delete('/delCategory', Category.delCategory)
-app.put('/updateCategory', Category.updateCategory)
-app.get('/showCategory', Category.showCategory)
-app.get('/indexCategory', Category.indexCategorys)
+app.post("/addCategory", Category.addCategory);
+app.delete("/delCategory", Category.delCategory);
+app.put("/updateCategory", Category.updateCategory);
+app.get("/showCategory", Category.showCategory);
+app.get("/indexCategory", Category.indexCategorys);
 // Products
-app.post('/createProduct', Products.createProds)
-app.put('/updateProducts', Products.updateProducts)
-app.delete('/deleteProducts', Products.deleteProducts)
-app.get('/showProduct',Products.showProduct)
-app.get('/indexProducts', Products.indexProducts)
-app.get('/indexByCategory', Products.indexByCategory)
+app.post("/createProduct", Products.createProds);
+app.put("/updateProducts", Products.updateProducts);
+app.delete("/deleteProducts", Products.deleteProducts);
+app.get("/showProduct", Products.showProduct);
+app.get("/indexProducts", Products.indexProducts);
+app.get("/indexByCategory", Products.indexByCategory);
+//admins
+app.post("/adminlogin", AdminLogin.adminLogin);
 
+app.get("/v1/totalorganizations", Login.totalOrganisations);
 
-
-app.listen(8050, ()=>{
-    console.log('Connected to port 8050')
-})
-
-
+app.listen(8050, () => {
+  console.log("Connected to port 8050");
+});
--- routes/login.ts
+++ routes/login.ts
@@ -1,57 +1,61 @@
-import * as express from 'express'
-import {knex} from "../Database/AdminDb"
-import * as jwt from 'jsonwebtoken'
-import * as bcrypt from 'bcryptjs'
-import { is } from 'sequelize/types/lib/operators';
-const jwtSecret = 'LHJHE12435544gffgg'
+import * as express from "express";
+import { knex } from "../Database/AdminDb";
+import * as jwt from "jsonwebtoken";
+import * as bcrypt from "bcryptjs";
+import { is } from "sequelize/types/lib/operators";
+const jwtSecret = "LHJHE12435544gffgg";
 
-class Login{
+class Login {
+  async totalOrganisations(req: any, res: any) {
+    return res.json({
+      instituicoesCadastradas: 848,
+      instituicoesTransmissoras: 153,
+    });
+  }
 
-   async trueLogin (req:any, res: any){
-        const dadosLogin = await req.body;
-        
-        const query1 = await knex.select()
-       .table('users')
-       .where({email: dadosLogin.email})
-      
-       if(query1.length >0){
-           const senhacorreta = bcrypt.compareSync(dadosLogin.password,query1[0].password)
-           
-            if(senhacorreta){
+  async trueLogin(req: any, res: any) {
+    const dadosLogin = await req.body;
 
-                jwt.sign({email: dadosLogin.email}, jwtSecret, {expiresIn:'2h'}, (err,token)=>{
-                    if(err){
-                        
-                        res.status(400)
-                        res.json({err: "falha ao gerar Token"})
-                    }
-                    console.log('Login Aceito')
-                    res.status(200)
-                    //window.localStorage.setItem('token', token)
-                    res.json({token: token})
-                })
-                    
-                
-            } else {
-                console.log('Senha invalida')
-                res.status(403).json({Error: 'Email Invalido'})
-              
-                res.redirect('/')
-            }    
+    const query1 = await knex
+      .select()
+      .table("users")
+      .where({ email: dadosLogin.email });
 
-       } else {
-           console.log('Email invalido ou nao encontrado')
-           res.status(400).json({error:'Email Invalido'})
-           
-          
-           res.redirect('/')
-           
-       }
-           
-     
-           
-   }       
-     
+    if (query1.length > 0) {
+      const senhacorreta = bcrypt.compareSync(
+        dadosLogin.password,
+        query1[0].password
+      );
+
+      if (senhacorreta) {
+        jwt.sign(
+          { email: dadosLogin.email },
+          jwtSecret,
+          { expiresIn: "2h" },
+          (err, token) => {
+            if (err) {
+              res.status(400);
+              res.json({ error: "falha ao gerar Token" });
+            }
+            console.log("Login Aceito");
+            res.status(200);
+
+            res.json({ token: token });
+          }
+        );
+      } else {
+        console.log("Senha invalida");
+        res.status(403);
+        res.json({ error: "Senha Invalida" });
+      }
+    } else {
+      console.log("Email invalido ou nao encontrado");
+      res.status(400);
+      res.json({ error: "Email Invalido" });
+
+      //res.redirect('/')
+    }
+  }
 }
 
-export default new Login()
+export default new Login();
